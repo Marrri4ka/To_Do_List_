515d4009aa534df362435c9af16b99af538b62ed
Mariia <mariiapopovych@gmail.com>
2019-06-09 00:32:36 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Task} from './models/task.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  tasks: Task []=[
+  new Task("Finish homework"),
+  new Task("Finish homework"),
+  new Task("Finish homework")
+
+];
+}
